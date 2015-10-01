1125a24f323e9a1e87f0f072b55cf153cdae67af
Pavel Kryukov <kryukov@frtk.ru>
2015-10-01 23:23:42 +0300

    Fix filters build with MSVS

--- filters/build.cmd
+++ filters/build.cmd
@@ -4,7 +4,8 @@ if "%1"=="gcc" (
     echo Building as_kryukov.dll with gcc
     g++ ./source/*.cpp -Wall -Werror -O3 -std=c++98 -pedantic -DBOOST_THREAD_USE_LIB -lboost_thread-mgw46-mt-1_47 --shared -Wl,--subsystem,windows -o ./bin/as_kryukov.dll
 ) else (
+    mkdir bin
     echo Building as_kryukov.dll with MSVC
-    cl /nologo /D_USRDLL /D_WINDLL /O2 /Ox /W4 /MD .\source\*.cpp /link /DLL /out:"bin\as_kryukov.dll"
+    cl /nologo /D_USRDLL /D_WINDLL /O2 /Ox /W4 /MD /EHsc .\source\*.cpp /link /DLL /out:"bin\as_kryukov.dll"
     del *.obj
 )
